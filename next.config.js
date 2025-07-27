880d3a892b4a0b923f167fdba8a181d269f07a70
ericpp-peng <ericpp.peng@gmail.com>
2025-07-27 22:52:45 +0800

    Upgrade Next.js to 15.4.4 and TypeScript to 5.8.3 for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,44 +1,11 @@
 /* eslint-env node */
 
-// https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
-const nextConfig = {
-  webpack: config => {
-    const oneOfRule = config.module.rules.find(rule => rule.oneOf);
-
-    // Next 12 has multiple TS loaders, and we need to update all of them.
-    const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
-
-    tsRules.forEach(rule => {
-      // eslint-disable-next-line no-param-reassign
-      rule.include = undefined;
-    });
-
-    return config;
-  },
-  compress: true,
-  generateEtags: true,
-  pageExtensions: ['tsx', 'mdx', 'ts'],
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-  svgo: {
-    multipass: true,
-    plugins: ['removeDimensions'],
-  },
-  strictMode: true,
-  trailingSlash: false,
-  images: {
-    unoptimized: true,
-  },
-};
-
 // eslint-disable-next-line no-undef
 module.exports = {
   // eslint-disable-next-line no-undef
   env: {
     customKey: 'my-value',
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/personal-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-website/' : '',
   // eslint-disable-next-line no-undef
   eslint: {
     ignoreDuringBuilds: true,
@@ -49,6 +16,7 @@ module.exports = {
   },
   // eslint-disable-next-line no-undef
   strictMode: true,
+  swcMinify: false,
   trailingSlash: false,
   images: {
     unoptimized: true,
